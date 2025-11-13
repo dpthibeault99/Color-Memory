83ee861e716fd75b9fed0a00e338592c51e6058f
dpthibeault99 <dpthibeault@email.neit.edu>
2025-11-13 10:26:16 -0500

    in class

---
+++ game.js
@@ -0,0 +1,31 @@
+import { Card } from "./card";
+
+let canvas = document.getElementById("myCanvas");
+let pencil = canvas.getContext("2d");
+
+
+let card = new Card (canvas,pencil);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import the Star class
+// import { Card } from "./card.js";
+
+// // star array
+
+// let cards = [];
+// let myCard = new Card(canvas, pencil);
+// stars.push(myStar);
+// ^^^ taken from starfield ^^^
